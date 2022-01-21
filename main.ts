1031e463c0ca3695a11c4cdbba203553cd2a8e93
Yuta Nakayama <nkymut@gmail.com>
2022-01-21 10:26:17 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,55 @@
 function normalFace () {
-    basic.showIcon(IconNames.Asleep)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
 }
+input.onButtonPressed(Button.A, function () {
+    hunger = hunger - 1
+    if (hunger < 0) {
+        hunger = 0
+    }
+    serial.writeValue("hunger", hunger)
+})
 function sadFace () {
     basic.showIcon(IconNames.Sad)
 }
+input.onButtonPressed(Button.B, function () {
+    hunger = hunger + 1
+    if (hunger > 10) {
+        hunger = 10
+    }
+    serial.writeValue("hunger", hunger)
+})
 function happyFace () {
     basic.showIcon(IconNames.Happy)
 }
+let hunger = 0
 let status = "happy"
+hunger = 10
 basic.forever(function () {
+    if (hunger < 3) {
+        status = "sad"
+    } else if (hunger >= 3 && hunger <= 6) {
+        status = "normal"
+    } else if (hunger < 6) {
+        status = "happy"
+    }
     if (status == "happy") {
         happyFace()
     } else if (status == "sad") {
         sadFace()
-    } else {
+    } else if (status == "normal") {
         normalFace()
     }
+})
+loops.everyInterval(3000, function () {
+    hunger = hunger - 1
+    if (hunger < 0) {
+        hunger = 0
+    }
+    serial.writeValue("hunger", hunger)
 })
